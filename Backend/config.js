0d2e11b3a4cfff7758bde1b14f3d555af4082342
Priya Khadke <priyakhadke15@gmail.com>
2019-12-08 21:47:09 -0800

    Congif.js

--- Backend/config.js
+++ Backend/config.js
@@ -1,12 +1,12 @@
 module.exports = {
-    jwtsecret: "",
-    encrAlgorithm: "",
-    encrSecret: "",
-    sql_host: '',
-    sql_port: "",
-    sql_user: '',
-    sql_password: '',
-    sql_database: '',
+    jwtsecret: "knuvv76u188zd2xu8c4xa",
+    encrAlgorithm: "aes256",
+    encrSecret: "1hmmp2sk8owpg8mtxxe8a",
+    sql_host: '192.168.0.6',
+    sql_port: "3306",
+    sql_user: 'admin',
+    sql_password: 'mypass',
+    sql_database: 'GrubHub',
     sql_connectionLimit: 50,
     initDb: process.env.INITDB === "true"
 };
